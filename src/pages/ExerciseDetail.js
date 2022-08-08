8469a6c10aa47c5f8dd0ef297439d93f0c0e625d
Marac Lupanciuc <marclupanc@gmail.com>
2022-08-08 09:02:16 +0300

    App file structure, header component, home page component with hero-section component, fetch setup in utils to fetch exercises

---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Box } from '@mui/material';
+
+
+const ExerciseDetail = () => {
+return (
+    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+     
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
