4a0f3a08012b4d9a1055f60fd457cfc80b1f6778
Marac Lupanciuc <marclupanc@gmail.com>
2022-08-12 14:59:17 +0300

    Setting up data fetching from rapidApi with the list of all exercises, search based on more parameters as name, target, equipment, body parts.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { Box } from '@mui/material';
+import React, { useState } from "react";
+import { Box } from "@mui/material";
 
-import Exercises from '../components/Exercises';
-import SearchExercises from '../components/SearchExercises';
-import HeroBanner from '../components/HeroBanner';
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
   const [exercises, setExercises] = useState([]);
-  const [bodyPart, setBodyPart] = useState('all');
+  const [bodyPart, setBodyPart] = useState("all");
 
   return (
     <Box>
       <HeroBanner />
-     
+      <SearchExercises />
     </Box>
   );
 };
