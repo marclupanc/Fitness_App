6eb91d4361e54737d4dc19d97f17101f6e75bb2d
Marc Lupanciuc <marclupanc@gmail.com>
2024-05-03 20:26:29 +0300

    testing telegram

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,6 +3,7 @@ import { Stack, Typography } from "@mui/material";
 import Icon from "../assets/icons/gym.png";
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
+  console.log("test");
   return (
     <Stack
       type="button"
